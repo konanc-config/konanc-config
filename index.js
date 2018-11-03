2f6ff5f4a901dde6499c12ced0a262b0c0cd4d70
Joseph Werle <joseph.werle@gmail.com>
2018-11-03 19:51:18 -0400

    feat(index.js): Introduce dependency requires

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const { extname, dirname, resolve } = require('path')
+const { existsSync } = require('fs')
 const { parse } = require('rc/lib/utils')
 const rc = require('rc')
 
@@ -15,13 +16,80 @@ function load(name, defaults, env) {
     env = {}
   }
 
-  return rc(name, defaults, { config }, (content) => {
+  const conf = rc(name, defaults, { config }, (content) => {
     Object.assign(env, process.env, {
       __dirname: resolve(dirname(config)),
       __filename: resolve(config),
     })
     return parse(template(content))
   })
+
+  if (conf.require && conf.require.length) {
+    if (!Array.isArray(conf.require)) {
+      conf.require = [ conf.require ]
+    }
+
+    const repos = (conf.repo && !Array.isArray(conf.repo)
+      ? [ conf.repo ]
+      : conf.repo)
+
+    for (const dep of conf.require) {
+      merge(conf, load(dep, defaults, env))
+      for (const repo of repos) {
+        merge(conf, load(resolve(repo, dep), defaults, env))
+      }
+    }
+  }
+
+  return conf
+
+  function merge(target, src) {
+    if (!target || !src ) {
+      return
+    }
+
+    if ('object' != typeof target || 'object' != typeof src) {
+      return
+    }
+
+    for (const k in src) {
+      if (Array.isArray(src[k])) {
+        if (Array.isArray(conf[k])) {
+          for (const x of src[k]) {
+            if (!conf[k].includes(x)) {
+              conf[k].push(x)
+            }
+          }
+        } else if ('string' == typeof conf[k]) {
+          if (src[k].includes(conf[k])) {
+            conf[k] = src[k]
+          } else {
+            conf[k] = [ conf[k] ].concat(src[k])
+          }
+        } else {
+          conf[k] = src[k]
+        }
+      } else if ('string' == typeof src[k]) {
+        if (Array.isArray(conf[k])) {
+          if (!conf[k].includes(src[k])) {
+            conf[k].push(src[k])
+          }
+        } else if ('string' == typeof conf[k]) {
+          if (conf[k] != src[k]) {
+            conf[k] = [ conf[k], src[k] ]
+          }
+        }
+      } else if ('object' == typeof src[k]) {
+        if ('object' == typeof target[k]) {
+          merge(src[k], target[k])
+        } else if (null != target[k] && 'undefined' != typeof target[k]) {
+          throw new TypeError("Cannot merge source object into target string.")
+        } else {
+          target[k] = src[k]
+        }
+      }
+    }
+  }
 
   function template(content) {
     const vars = Object.keys(env)
