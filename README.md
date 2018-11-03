2c7e42e1f7585c1c019a1c8067a4e29d8a83cd23
Joseph Werle <joseph.werle@gmail.com>
2018-11-03 13:36:56 -0400

    feat(.): Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,50 @@
-# konanc-config
-Command line utility to read '*.kc' konanc config files to print compiler flags
+konanc-config
+=============
+
+Command line utility to read `*.kc` konanc config files to print compiler flags.
+
+## Installation
+
+```sh
+$ npm install @datkt/konanc-config
+```
+
+## Usage
+
+```sh
+usage: konanc-config: [-hV] [options] <configs>
+
+where options can be:
+
+  -h, --help                    Print this message
+  -V, --version                 Print program version
+  -c, --cflags                  Print compiler flags
+  -r, --repos, --repositories   Print repositories
+  -l --libs, --libraries        Print libraries
+
+```
+
+## Example
+
+```sh
+$ konanc-config module.kc --libraries --repos --cflags
+-opt -r /home/werle/repos/datkt/konanc-config/node_modules/@datkt -l sodium/sodium -l tape/tape
+```
+
+where `module.kc` is
+
+```ini
+library[] = sodium/sodium
+library[] = tape/tape
+cflags[] = -opt
+repo[] = "${__dirname}/node_modules/@datkt"
+```
+
+## See Also
+
+* https://github.com/pkgconf/pkgconf
+* https://github.com/Distrotech/pkg-config
+
+## License
+
+MIT
---
+++ index.js
@@ -0,0 +1,36 @@
+const { extname, dirname, resolve } = require('path')
+const { parse } = require('rc/lib/utils')
+const rc = require('rc')
+
+module.exports = load
+module.exports.load = load
+
+function load(name, defaults, env) {
+  if (!name || 'string' != typeof name) {
+    throw new TypeError("Expecting name to be a non-empty string.")
+  }
+
+  const config = '.kc' == extname(name) ? name : `${name}.kc`
+  if (!env || 'object' != typeof env) {
+    env = {}
+  }
+
+  return rc(name, defaults, { config }, (content) => {
+    Object.assign(env, process.env, {
+      __dirname: resolve(dirname(config)),
+      __filename: config,
+    })
+    return parse(template(content))
+  })
+
+  function template(content) {
+    const vars = Object.keys(env)
+    const decls = vars.map((key) => `const ${key} = ${key}_value;`).join('\n')
+    const values = vars.map((key) => env[key])
+    const params = vars.map((key) => `${key}_value`)
+    const interop = '\n return (`' + content + '`);'
+    const generator = Function.apply(null, params.concat(decls + interop))
+
+    return generator.apply(null, values)
+  }
+}
